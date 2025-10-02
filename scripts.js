2215c35b130b5bbd94774142c63d1359fdc16502
gcamargocoder <gcamargocoder@gmail.com>
2025-10-02 01:07:49 -0300

    Update

--- scripts.js
+++ scripts.js
@@ -1,12 +1,21 @@
+let copo = document.querySelector(".copo-starbucks");
+let circulo = document.querySelector(".circulo");
 
-let imagem = document.querySelector(".copo-starbucks")
-let fundo = document.querySelector(".circulo")
+function trocaCopo(imagemSrc, cor) {
+    // Atualiza a imagem do copo
+    copo.src = imagemSrc;
 
-function trocaImagem(endereco){
-    imagem.src = endereco
+    // Adiciona animação de "crescer e voltar"
+    copo.classList.remove("animar-copo");
+    void copo.offsetWidth; // força reflow
+    copo.classList.add("animar-copo");
+
+    // Atualiza a cor do círculo
+    circulo.style.background = cor;
+
+    // Efeito de expansão do círculo
+    circulo.style.transform = "scale(1.05)";
+    setTimeout(() => {
+        circulo.style.transform = "scale(1)";
+    }, 300);
 }
-
-function trocaFundo(cor){
-    fundo.style.background = cor
-}
-
